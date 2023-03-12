2a840c11341d61004836548de17dc8a59102acc2
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-03-12 21:57:34 +0100

    Cooking

--- frontend/src/Components/Index.js
+++ frontend/src/Components/Index.js
@@ -1,7 +1,13 @@
-import Header from './Header';
+import Card from './Card';
 import Footer from './Footer';
+import FormField from './FormField';
+import Header from './Header';
+import Loader from './Loader';
 
 export {
-    Header,
+    Card,
     Footer,
+    FormField,
+    Header,
+    Loader,
 };
