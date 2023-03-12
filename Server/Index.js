52a105cea06bb3af4a3798ba12dedc1fd17615b8
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-03-13 00:56:54 +0100

    Cooking

--- Server/Index.js
+++ Server/Index.js
@@ -12,6 +12,9 @@ const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
 app.get('/', async (req, res) => {
     res.status(200).json({
         message: 'Heyy there, This is a Cat ',
@@ -24,10 +27,14 @@ process.env.STATUS === "development"
     : (PORT = process.env.PROD_ENDPOINT_URL)
 
 async function StartServer() {
-
-    app.listen(PORT, () => {
-        console.log(`Server is in ${process.env.STATUS} mode, listening on port ${PORT}`)
-    })
+    try {
+        connectDB(process.env.MONGODB_URL)
+        app.listen(PORT, () => {
+            console.log(`Server is in ${process.env.STATUS} mode, listening on port ${PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 StartServer()
